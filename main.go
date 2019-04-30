4276bbff79a4dd4a434c3444224db6195ca8a60d
piero de salvia <pmdesalvia@tf1.fr>
2019-04-30 11:31:18 +0200

    Create input routine

--- main.go
+++ main.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var helpFlag = flag.Bool("help", false, "")
 
 func main() {
-	fmt.Print("Enter input file name/path: ")
+	flag.Parse()
+	if *helpFlag {
+		fmt.Println("Usage: mgaen clearfile encryptedfile")
+		return
+	}
+	inFile := ""
+	outFile := ""
+	switch len(os.Args) {
+	case 1:
+		fmt.Print("Enter clear file name/path: ")
+		consoleReader := bufio.NewReader(os.Stdin)
+		inFile, _ = consoleReader.ReadString('\n')
+		inFile = strings.Replace(inFile, "\n", "", -1)
+
+		fmt.Print("Enter encrypted file name/path: ")
+		outFile, _ = consoleReader.ReadString('\n')
+		outFile = strings.Replace(outFile, "\n", "", -1)
+
+	case 2:
+		inFile = os.Args[1]
+		fmt.Println("Using clear file " + inFile)
+
+		consoleReader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter encrypted file name/path: ")
+		outFile, _ = consoleReader.ReadString('\n')
+		outFile = strings.Replace(outFile, "\n", "", -1)
+
+	case 3:
+		inFile = os.Args[1]
+		fmt.Println("Using clear file " + inFile)
+
+		outFile := os.Args[2]
+		fmt.Println("Creating encrypted file " + outFile)
+	}
 
 }
